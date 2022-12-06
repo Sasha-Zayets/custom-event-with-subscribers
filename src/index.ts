84cd34b64388a548004e266c4ad2f2679278ac29
OFFICEUKRAINE\ole.zaiets <ole.zaiets@temabit.com>
2022-12-06 13:09:04 +0200

    Folder structure changed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-export * from "./customEvent";
+type Callback = (data: any) => void;
+
+export const customEvent = {
+    createEvent(nameEvent: string, params: any): CustomEvent {
+        return new CustomEvent(nameEvent, {
+            bubbles: false,
+            detail: params,
+        });
+    },
+
+    dispatch(nameEvent: string, params: any): void {
+        const event = this.createEvent(nameEvent, params);
+        window.dispatchEvent(event);
+    },
+
+    subscribe(event: string, callback: Callback) {
+        window.addEventListener(event, (e: CustomEvent) => {
+            const data = e.detail;
+            callback && callback(data);
+        });
+    },
+
+    unsubscribe(event: string) {
+        window.removeEventListener(event, () => {
+            this.removeSubscribe(event);
+        });
+    },
+};
+
+export const dispatch = customEvent.dispatch;
+export const subscribe = customEvent.subscribe;
+export const unsubscribe = customEvent.unsubscribe;
